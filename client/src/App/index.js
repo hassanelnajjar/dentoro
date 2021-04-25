af2cdc9d2ce68cf5f74f62247eb113376c39afec
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-25 12:17:06 +0300

    apply lazy loading - web socket to update the today sechule when adding new appointment

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,61 +1,67 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { PrivateRoutes, LoggedOutRoutes } from './Routes';
 
-import Patients from '../components/Patients';
-import LoginPage from '../pages/logIn';
-import Sidebar from '../components/Sidebar';
-import Calendar from '../pages/admin/Calendar';
-import PatientProfile from '../pages/admin/PatientProfile';
-import LandingPage from '../pages/LandingPage';
-
-import PatientsAppointmentTable from '../pages/admin/PatientsAppointmentTable';
-
 import './App.css';
+import Loading from '../components/common/Loading';
+
+const LoginPage = lazy(() => import('../pages/logIn'));
+const Sidebar = lazy(() => import('../components/Sidebar'));
+const Calendar = lazy(() => import('../pages/admin/Calendar'));
+const PatientProfile = lazy(() => import('../pages/admin/PatientProfile'));
+const LandingPage = lazy(() => import('../pages/LandingPage'));
+
+const PatientsAppointmentTable = lazy(() =>
+  import('../pages/admin/PatientsAppointmentTable')
+);
+
+const Patients = lazy(() => import('../components/Patients'));
 
 const App = () => (
   <div className="App">
-    <Switch>
-      <Route exact path="/">
-        <LandingPage />
-      </Route>
-      <LoggedOutRoutes exact path="/login">
-        <LoginPage />
-      </LoggedOutRoutes>
-      <PrivateRoutes path="/dashboard">
-        <Sidebar>
-          <Switch>
-            <Route exact path="/dashboard">
-              <PatientsAppointmentTable
-                showSearchBar={false}
-                pageTitle="Today's Schedule"
-              />
-            </Route>
-            <Route exact path="/dashboard/calendar">
-              <Calendar />
-            </Route>
-            <Route exact path="/dashboard/calendar/appointmentsearch">
-              <PatientsAppointmentTable pageTitle="Patients Appointment Table" />
-            </Route>
-            <Route exact path="/dashboard/calendar/:appointmentDate">
-              <PatientsAppointmentTable pageTitle="Patients Appointment Table" />
-            </Route>
-            <Route exact path="/dashboard/patients">
-              <Patients />
-            </Route>
-            <Route exact path="/dashboard/patients/:patientId">
-              <PatientProfile />
-            </Route>
-            <Redirect to="/404" />
-          </Switch>
-        </Sidebar>
-      </PrivateRoutes>
-      <Route>
-        <h1>Error 404 Not Found !!</h1>
-      </Route>
-      <Redirect to="/404" />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <LoggedOutRoutes exact path="/login">
+          <LoginPage />
+        </LoggedOutRoutes>
+        <PrivateRoutes path="/dashboard">
+          <Sidebar>
+            <Switch>
+              <Route exact path="/dashboard">
+                <PatientsAppointmentTable
+                  showSearchBar={false}
+                  pageTitle="Today's Schedule"
+                />
+              </Route>
+              <Route exact path="/dashboard/calendar">
+                <Calendar />
+              </Route>
+              <Route exact path="/dashboard/calendar/appointmentsearch">
+                <PatientsAppointmentTable pageTitle="Patients Appointment Table" />
+              </Route>
+              <Route exact path="/dashboard/calendar/:appointmentDate">
+                <PatientsAppointmentTable pageTitle="Patients Appointment Table" />
+              </Route>
+              <Route exact path="/dashboard/patients">
+                <Patients />
+              </Route>
+              <Route exact path="/dashboard/patients/:patientId">
+                <PatientProfile />
+              </Route>
+              <Redirect to="/404" />
+            </Switch>
+          </Sidebar>
+        </PrivateRoutes>
+        <Route>
+          <h1>Error 404 Not Found !!</h1>
+        </Route>
+        <Redirect to="/404" />
+      </Switch>
+    </Suspense>
   </div>
 );
 
